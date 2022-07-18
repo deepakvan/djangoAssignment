919ececb34c0a325649c057fddd46467c66977ea
Deepakvan <deepakbmr07@gmail.com>
2022-07-18 13:14:24 +0530

    first commit

--- library/urls.py
+++ library/urls.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 from django.urls import path,include
 from .api import RegisterApi
 from . import views
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
 
     path('',views.home,name="home"),
@@ -21,5 +39,7 @@ urlpatterns = [
     path('api/deletemember', views.DeleteMember.as_view(), name='DeleteMember'),
     path('api/viewmember', views.ViewMember.as_view(), name='ViewMember'),
     path('api/viewallmember', views.ViewAllMember.as_view(), name='ViewAllMember'),
+
+    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
